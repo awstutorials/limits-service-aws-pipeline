c5dee3e906b2fac4c68db8159b5f5fb02ef09497
Ravisankar <ravisankar.munusamy@cognizant.com>
2019-07-19 11:36:43 +0200

    export port 80

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+echo 'helloworld...'
+echo '*************'
 export MINIMUM=$(aws ssm get-parameter --region eu-west-1 --name ${MINIMUM_PARAM_NAME} | jq -r .Parameter.Value)
 export MAXIMUM=$(aws ssm get-parameter --region eu-west-1 --name ${MAXIMUM_PARAM_NAME} | jq -r .Parameter.Value)
+
+echo $MINIMUM
+echo $MAXIMUM
+echo '*************'
 
 java -jar application.jar
